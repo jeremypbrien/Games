06a26ee0c3731d1e265fe39e3707b1cb30813006
Jeremy Brien <jbrien@purdue.edu>
2019-08-22 16:25:45 -0400

    Cleaned up code.

--- 2048.py
+++ 2048.py
@@ -1,6 +1,21 @@
 from tkinter import *
 from random import choice
 
+COLORS = {
+    "": "#cdc0b4",
+    "2": "#eee4da",
+    "4": "#ede0c8",
+    "8": "#f2b179",
+    "16": "#f59565",
+    "32": "#f67c5f",
+    "64": "#f65d3b",
+    "128": "#edce71",
+    "256": "#edcc63",
+    "512": "#edc750",
+    "1024": "#edc440",
+    "2048": "#eec22e"
+}
+
 
 class App:
 
@@ -43,11 +58,15 @@ class App:
         if direction == "left":
             for row in self.tiles:
                 values = [x.cget("text") for x in row if x.cget("text") != ""]
+
+                # Combine pairs into one from left to right
                 for i in range(1, len(values)):
                     if values[i] == values[i - 1]:
                         values[i - 1] = str(int(values[i - 1]) * 2)
                         values[i] = ""
                         self.update_score_by(values[i-1])
+
+                # Refill row with empty slots to fill board again
                 values = [x for x in values if x != ""]
                 while len(values) < 4:
                     values.append("")
@@ -57,12 +76,16 @@ class App:
         elif direction == "right":
             for row in self.tiles:
                 values = [x.cget("text") for x in row if x.cget("text") != ""]
+
+                # Combine pairs into one from right to left
                 for i in reversed(range(len(values) - 1)):
                     if values[i] == values[i + 1]:
                         values[i + 1] = str(int(values[i + 1]) * 2)
                         values[i] = ""
                         self.update_score_by(values[i + 1])
                 values = [x for x in values if x != ""]
+
+                # Refill row from start with empty slots to fill board again
                 while len(values) < 4:
                     values.insert(0, "")
                 for i in range(4):
@@ -71,12 +94,16 @@ class App:
         elif direction == "up":
             for index in range(4):
                 values = [row[index].cget("text") for row in self.tiles if row[index].cget("text") != ""]
+
+                # Combine pairs into one from top to bottom
                 for i in range(1, len(values)):
                     if values[i] == values[i - 1]:
                         values[i - 1] = str(int(values[i - 1]) * 2)
                         values[i] = ""
                         self.update_score_by(values[i-1])
                 values = [x for x in values if x != ""]
+
+                # Refill column from bottom with empty slots to fill board again
                 while len(values) < 4:
                     values.append("")
                 for row in range(4):
@@ -85,17 +112,22 @@ class App:
         elif direction == "down":
             for index in range(4):
                 values = [row[index].cget("text") for row in self.tiles if row[index].cget("text") != ""]
+
+                # Combine pairs into one from bottom to top
                 for i in reversed(range(len(values) - 1)):
                     if values[i] == values[i + 1]:
                         values[i + 1] = str(int(values[i + 1]) * 2)
                         values[i] = ""
                         self.update_score_by(values[i + 1])
                 values = [x for x in values if x != ""]
+
+                # Refill column from top with empty slots to fill board again
                 while len(values) < 4:
                     values.insert(0, "")
                 for row in range(4):
                     self.tiles[row][index].config(text=values[row])
 
+        # Add new tile for next turn and check if moves are available
         if old_values != [[z.cget("text") for z in y] for y in [x for x in self.tiles]]:
             self.add_random_tile()
             if self.detected_loss():
@@ -121,30 +153,9 @@ class App:
             for col in range(4):
                 tile = self.tiles[row][col]
                 tile_text = self.tiles[row][col].cget("text")
-                if tile_text == "":
-                    tile.config(bg="#cdc0b4")
-                elif tile_text == "2":
-                    tile.config(bg="#eee4da")
-                elif tile_text == "4":
-                    tile.config(bg="#ede0c8")
-                elif tile_text == "8":
-                    tile.config(bg="#f2b179")
-                elif tile_text == "16":
-                    tile.config(bg="#f59565")
-                elif tile_text == "32":
-                    tile.config(bg="#f67c5f")
-                elif tile_text == "64":
-                    tile.config(bg="#f65d3b")
-                elif tile_text == "128":
-                    tile.config(bg="#edce71")
-                elif tile_text == "256":
-                    tile.config(bg="#edcc63")
-                elif tile_text == "512":
-                    tile.config(bg="#edc750")
-                elif tile_text == "1024":
-                    tile.config(bg="#edc440")
-                elif tile_text == "2048":
-                    tile.config(bg="#eec22e")
+                color = COLORS.get(tile_text)
+                if color is not None:
+                    tile.config(bg=color)
                 else:
                     tile.config(bg="red")
 
@@ -162,6 +173,7 @@ class App:
                     if i == values[row][col]:
                         return False
         return True
+
 
 if __name__ == "__main__":
     app = App()
